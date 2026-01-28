f41f36e17e881320747043ab798447c443ebc00c
Daniel <daniel.ast2007@gmail.com>
2026-01-28 10:59:19 +0300

    copypasted load_encoding

--- main.c
+++ main.c
@@ -12,8 +12,8 @@ static inline size_t len(const char *str)
 }
 
 static void	load_encoding(pdfio_obj_t *page_obj, const char *name, int encoding[256]);
-static void	put_utf8(int ch);
-static void	puts_utf16(const char *s);
+static void	put_utf8(int ch, FILE *stream);
+static void	puts_utf16(const char *s, FILE *stream);
 
 int main(int argc, char *argv[])
 {
@@ -76,17 +76,24 @@ int main(int argc, char *argv[])
 			else if(buffer[0] == '(')
 			{
 				first = 0;
-				for(bufptr = buffer + 1; *bufptr; bufptr++) put_utf8(encoding[*bufptr & 255]);
+				for(bufptr = buffer + 1; *bufptr; bufptr++) put_utf8(encoding[*bufptr & 255], output);
 			}
 			else if(buffer[0] == '<')
 			{
 				first = 0;
-				puts_utf16(buffer + 1);
+				puts_utf16(buffer + 1, output);
 			}
 			else if(buffer[0] == '/')
 			{
+				strncpy(font, buffer + 1, sizeof(font) - 1);
+				font[sizeof(font) - 1] = '\0';
 			}
-
+			else if(!strcmp(buffer,"Tf") && font[0]) load_encoding(page, font, encoding);
+			else if(!strcmp(buffer, "Td") || !strcmp(buffer, "TD") || !strcmp(buffer, "T*") || !strcmp(buffer, "\'") || !strcmp(buffer, "\""))
+			{
+				putchar('\n');
+				first = 1;
+			}
 		}
 	}
 
@@ -95,4 +102,74 @@ int main(int argc, char *argv[])
 	pdfioFileClose(pdf);
 
 	return 0;
+}
+
+static void load_encoding(pdfio_obj_t *pageObj, const char *font, int encoding[256])
+{
+	pdfio_dict_t *pageDict, *resourcesDict, *fontDict;
+	pdfio_obj_t *fontObj, *encodingObj;
+	static int win_ansi[32];
+	for(int i  = 128; i <= 159; i++) win_ansi[i-128] = i;
+	static int mac_roman[128];
+	for(int i = 128; i <= 255; i++) mac_roman[i-128] = i;
+
+	for(int i = 0; i < 128; i++) encoding[i] = i;
+	for(int i = 160; i < 256; i++) encoding[i] = i;
+	memcpy(encoding + 128, win_ansi, sizeof(win_ansi));
+
+	if((pageDict = pdfioObjGetDict(pageObj)) == NULL) return;
+	if((resourcesDict = pdfioDictGetDict(pageDict, "Resources")) == NULL) return;
+	if((fontDict = pdfioDictGetDict(resourcesDict, "Font")) == NULL)
+	{
+		if((fontObj = pdfioDictGetObj(resourcesDict, "Font")) == NULL) 
+			fontDict = pdfioObjGetDict(fontObj);
+		if(!fontDict) return;
+	}
+	if((fontObj = pdfioDictGetObj(fontDict, font)) == NULL) return;
+
+	pdfio_dict_t *encodingDict;
+
+	if((encodingObj = pdfioDictGetObj(pdfioObjGetDict(fontObj), "Encoding")) == NULL) return;
+	if((encodingDict = pdfioObjGetDict(encodingObj)) == NULL) return;
+
+	const char *baseEncoding;
+	pdfio_array_t *differences;
+
+	baseEncoding = pdfioDictGetName(encodingDict, "BaseEncoding");
+	differences = pdfioDictGetArray(encodingDict, "Differences");
+
+	if(baseEncoding && !strcmp(baseEncoding, "MacRomanEncoding")) 
+		memcpy(encoding + 128, mac_roman, sizeof(mac_roman));
+
+	if(differences)
+	{
+		size_t count = pdfioArrayGetSize(differences);
+		const char *name;
+		size_t idx = 0;
+
+		for(size_t i = 0; i < count; i++)
+		{
+			switch (pdfioArrayGetType(differences, i)) 
+			{
+				case PDFIO_VALTYPE_NUMBER:
+					idx = (size_t)pdfioArrayGetNumber(differences, i);
+					break;
+				
+				case PDFIO_VALTYPE_NAME:
+					if(idx < 0 || idx > 255) break;
+
+					name = pdfioArrayGetName(differences, i);
+					for(size_t j=0; j< (sizeof(unicode_map)/sizeof(unicode_map[0])); j++)
+						if(!strcmp(name, unicode_map[j].name))
+						{
+							encoding[idx] = unicode_map[j].unicode;
+							break;
+						}
+					idx++;
+					break;
+				
+				default: break;
+			}
+		}
+	}
 }
